9a30a7d8da10c4efc2985dabcfa7752ca1efc9f9
Luis Reyna <luisantoinfo@gmail.com>
2020-07-13 12:57:36 +0200

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListUserComponent } from './components/list-user/list-user.component';
+import { HeaderComponent } from './components/header/header.component';
+
+// ADD ANGULAR MATERIAL
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListUserComponent
+    ListUserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatSliderModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/persona.type.ts
@@ -0,0 +1,8 @@
+export interface Persona {
+    nombre: string;
+    apellido1: string;
+    apellido2: string;
+    genero: string;
+    fechaNacimiento: Date;
+    nifPasaporte: string;
+}
